e3f89ed97d1d23617289fe7c0a6df0d72aceabd2
Aiden <aiden@bordergiant,com>
2024-06-07 17:56:32 -0400

    initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MyButton from "./components/Button";
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
+
+
+export default MyApp
